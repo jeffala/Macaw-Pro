9136eb4201c21183aeea0e1dc9ea5fe434a67a58
Jeff Licona <jeffalalg94@gmail.com>
2022-03-04 22:14:12 -0500

    initial work for add profile picture feature

--- Macaw Pro/Views/AccountView/AccountView.swift
+++ Macaw Pro/Views/AccountView/AccountView.swift
@@ -4,58 +4,48 @@ import Firebase
 import GoogleSignIn
 
 
-
 struct AccountView: View {
     @Environment(\.userSignIn) private var userSignIn: Binding<Bool>
-    @State private var isLoggedOut = false
+    @State private var isShowingPhotoPicker = false
     
-    var user: User = User(id: "000", firstName: "Jeff", lastName: "Licona", gender: "Male", proType: "Barber", email: "k=jdfd", service1Name: "Pickle Slap", service1Price: 100)
+    // This is the Image that needs to go to FireStore
+    @State private var avatarImage = UIImage(named: "default-avatar")!
+    
+//    @State private var isLoggedOut = false
+    
+    
+    var user: User = User(id: "000", firstName: "Jeff", lastName: "Licona", gender: "Male", genderOptions: ["Male", "Female", "Decline to answer"], proType: "Barber", email: "jeffalalg94@gmail.com")
         
     
     var body: some View {
         NavigationView {
             VStack(spacing: 35 ) {
                 HStack {
-                    Image(systemName: "person.fill")
+                    Image(uiImage: avatarImage)
                         .resizable()
+                        .scaledToFit()
                         .frame(width: 150, height: 150)
                         .clipShape(Circle())
-                        .redacted(reason: .placeholder)
+                        .padding()
+                        .onTapGesture { isShowingPhotoPicker = true }
+                    
                     
                     VStack(spacing: 8) {
                         Text("\(user.firstName + " " + user.lastName)")
-                            .font(.headline)
+                        
                         Text("\(user.proType)")
-                            .font(.subheadline)
                     }
                 }
-                Text("Services")
+                Text("My Services")
+                    .font(.title2)
+                Spacer()
             }
             .navigationTitle("Account")
+            .sheet(isPresented: $isShowingPhotoPicker, content: {
+                PhotoPicker(avatarImage: $avatarImage)
+            })
         }
     }
-    
-    //    Button("Log Out") {
-    //
-    //        // Log out User...
-    ////            DispatchQueue.global(qos: .background).async {
-    //            try? Auth.auth().signOut()
-    //
-    ////            }
-    //
-    //        // Google Log out User...
-    //        GIDSignIn.sharedInstance.signOut()
-    //
-    //
-    //        // Taking user out of logged in view...
-    //        self.isLoggedOut = true
-    //
-    //        // Setting userSigned back to false
-    //        self.userSignIn.wrappedValue = false
-    //    }
-    //    .fullScreenCover(isPresented: $isLoggedOut, onDismiss: nil) {
-    //        WelcomeScreenView()
-    //    }
 }
 
 
@@ -67,17 +57,29 @@ struct AccountView_Previews: PreviewProvider {
 }
 
 
-extension Date {
-    init(_ dateString:String) {
-        let dateStringFormatter = DateFormatter()
-        dateStringFormatter.dateFormat = "yyyy-MM-dd"
-        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale
-        let date = dateStringFormatter.date(from: dateString)!
-        self.init(timeInterval:0, since:date)
-    }
-}
 
 
+//    Button("Log Out") {
+//
+//        // Log out User...
+////            DispatchQueue.global(qos: .background).async {
+//            try? Auth.auth().signOut()
+//
+////            }
+//
+//        // Google Log out User...
+//        GIDSignIn.sharedInstance.signOut()
+//
+//
+//        // Taking user out of logged in view...
+//        self.isLoggedOut = true
+//
+//        // Setting userSigned back to false
+//        self.userSignIn.wrappedValue = false
+//    }
+//    .fullScreenCover(isPresented: $isLoggedOut, onDismiss: nil) {
+//        WelcomeScreenView()
+//    }
 
 
 
--- Macaw Pro/Views/AccountView/EditAccountViewModel.swift
+++ Macaw Pro/Views/AccountView/EditAccountViewModel.swift
@@ -1,9 +1,3 @@
-//
-//  EditProfileViewModel.swift
-//  Macaw Pro
-//
-//  Created by Jeff Licona on 2/17/22.
-//
 
 import Foundation
 import Firebase
@@ -12,6 +6,8 @@ import FirebaseFirestore
 
 class EditAccountViewModel: ObservableObject {
     @Published var user = User()
+    @Published var services = Services()
+    
     private var db = Firestore.firestore()
     
     func saveData() {
@@ -22,7 +18,35 @@ class EditAccountViewModel: ObservableObject {
             "Profession": user.proType,
             "Email": user.email,
             "Services": [
-                user.service1Name : user.service1Price
+                "service1Name:" : services.service1Name,
+                "service1Price" : services.service1Price,
+                
+                "service2Name" : services.service2Name,
+                "service2Price" : services.service2Price,
+                
+                "service3Name" : services.service3Name,
+                "service3Price" : services.service3Price,
+                
+                "service4Name" : services.service4Name,
+                "service4Price" : services.service4Price,
+                
+                "service5Name" : services.service5Name,
+                "service5Price" : services.service5Price,
+                
+                "service6Name" : services.service6Name,
+                "service6Price" : services.service6Price,
+                
+                "service7Name" : services.service7Name,
+                "service7Price" : services.service7Price,
+                
+                "service8Name" : services.service8Name,
+                "service8Price" : services.service8Price,
+                
+                "service9Name" : services.service9Name,
+                "service9Price" : services.service9Price,
+                
+                "service10Name" : services.service10Name,
+                "service10Price" : services.service10Price
             ]
         ]) { err in
             if let err = err {
@@ -33,3 +57,4 @@ class EditAccountViewModel: ObservableObject {
         }
     }
 }
+
